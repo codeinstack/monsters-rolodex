f139160a2c7118776e8efbf1ea8b9d57a50512c6
codeinstack <codeinstack@gmail.com>
2020-08-12 20:54:33 +0530

    Updates

---
+++ precache-manifest.5e6a67710068a284ade3494ba030b495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ef012af1728a129281c5df11d0729f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c08c8149620ed274a6d9",
+    "url": "/monsters-rolodex/static/css/main.351e74e4.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08c8149620ed274a6d9",
+    "url": "/monsters-rolodex/static/js/main.7abd9c67.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
